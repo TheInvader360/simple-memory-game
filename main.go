1e2d14c38baad82028885e9b6b17020fb9a18a40
Darren <theinvader360@gmail.com>
2020-05-24 17:52:47 +0100

    Introduce pads

--- main.go
+++ main.go
@@ -1,58 +1,90 @@
 package main
 
 import (
-  "fmt"
-  "image/color"
-  "github.com/hajimehoshi/ebiten"
-  "github.com/hajimehoshi/ebiten/ebitenutil"
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten"
 )
 
-var square *ebiten.Image
+const (
+	screenSize, padSize = 480, 220
+)
+
+var (
+	blueDark    = &color.NRGBA{0x00, 0x00, 0x33, 0xff}
+	blueLight   = &color.NRGBA{0x00, 0x00, 0xff, 0xff}
+	greenDark   = &color.NRGBA{0x00, 0x33, 0x00, 0xff}
+	greenLight  = &color.NRGBA{0x00, 0xff, 0x00, 0xff}
+	redDark     = &color.NRGBA{0x33, 0x00, 0x00, 0xff}
+	redLight    = &color.NRGBA{0xff, 0x00, 0x00, 0xff}
+	yellowDark  = &color.NRGBA{0x33, 0x33, 0x00, 0xff}
+	yellowLight = &color.NRGBA{0xff, 0xff, 0x00, 0xff}
+
+	pads []pad
+)
+
+type pad struct {
+	x, y              float64
+	on                bool
+	imageOff, imageOn *ebiten.Image
+}
+
+func init() {
+	blueDarkImage, _ := ebiten.NewImage(padSize, padSize, ebiten.FilterDefault)
+	blueDarkImage.Fill(blueDark)
+	blueLightImage, _ := ebiten.NewImage(padSize, padSize, ebiten.FilterDefault)
+	blueLightImage.Fill(blueLight)
+	greenDarkImage, _ := ebiten.NewImage(padSize, padSize, ebiten.FilterDefault)
+	greenDarkImage.Fill(greenDark)
+	greenLightImage, _ := ebiten.NewImage(padSize, padSize, ebiten.FilterDefault)
+	greenLightImage.Fill(greenLight)
+	redDarkImage, _ := ebiten.NewImage(padSize, padSize, ebiten.FilterDefault)
+	redDarkImage.Fill(redDark)
+	redLightImage, _ := ebiten.NewImage(padSize, padSize, ebiten.FilterDefault)
+	redLightImage.Fill(redLight)
+	yellowDarkImage, _ := ebiten.NewImage(padSize, padSize, ebiten.FilterDefault)
+	yellowDarkImage.Fill(yellowDark)
+	yellowLightImage, _ := ebiten.NewImage(padSize, padSize, ebiten.FilterDefault)
+	yellowLightImage.Fill(yellowLight)
+
+	pads = append(pads, pad{0, 0, false, blueDarkImage, blueLightImage})
+	pads = append(pads, pad{screenSize - padSize, 0, false, greenDarkImage, greenLightImage})
+	pads = append(pads, pad{0, screenSize - padSize, false, redDarkImage, redLightImage})
+	pads = append(pads, pad{screenSize - padSize, screenSize - padSize, false, yellowDarkImage, yellowLightImage})
+}
 
 func update(screen *ebiten.Image) error {
-  screen.Fill(color.NRGBA{0x00, 0xff, 0x00, 0xff})
-  x, y := ebiten.CursorPosition()
-  ebitenutil.DebugPrint(screen, fmt.Sprintf("X: %d, Y: %d", x, y))
-  if !ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) && !ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
-    ebitenutil.DebugPrint(screen, "\n\nTry pressing LEFT MOUSE BUTTON or RIGHT MOUSE BUTTON")
-  }
-  if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-      ebitenutil.DebugPrint(screen, "\n\n\nPressing LEFT MOUSE BUTTON")
-  }
-  if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
-    ebitenutil.DebugPrint(screen, "\n\n\n\nPressing RIGHT MOUSE BUTTON")
-  }
-  if !ebiten.IsKeyPressed(ebiten.KeyUp) && !ebiten.IsKeyPressed(ebiten.KeyDown) && !ebiten.IsKeyPressed(ebiten.KeyLeft) && !ebiten.IsKeyPressed(ebiten.KeyRight) {
-    ebitenutil.DebugPrint(screen, "\n\n\n\n\n\nTry pressing UP/DOWN/LEFT/RIGHT cursor keys")
-  }
-  if ebiten.IsKeyPressed(ebiten.KeyUp) {
-    ebitenutil.DebugPrint(screen, "\n\n\n\n\n\n\nPressing UP")
-  }
-  if ebiten.IsKeyPressed(ebiten.KeyDown) {
-    ebitenutil.DebugPrint(screen, "\n\n\n\n\n\n\n\nPressing DOWN")
-  }
-  if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-    ebitenutil.DebugPrint(screen, "\n\n\n\n\n\n\n\n\nPressing LEFT")
-  }
-  if ebiten.IsKeyPressed(ebiten.KeyRight) {
-    ebitenutil.DebugPrint(screen, "\n\n\n\n\n\n\n\n\n\nPressing RIGHT")
-  }
-  if square == nil {
-    square, _ = ebiten.NewImage(32, 32, ebiten.FilterNearest)
-  }
-  opts := &ebiten.DrawImageOptions{}
-  opts.GeoM.Translate(128, 128)
-  square.Fill(color.Black)
-  screen.DrawImage(square, opts)
-  opts.GeoM.Translate(-32, -64)
-  opts.GeoM.Scale(2, 2)
-  square.Fill(color.White)
-  screen.DrawImage(square, opts)
-  return nil
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		pads[0].on = true
+		pads[1].on = false
+		pads[2].on = false
+		pads[3].on = true
+	} else {
+		pads[0].on = false
+		pads[1].on = true
+		pads[2].on = true
+		pads[3].on = false
+	}
+
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+
+	for _, pad := range pads {
+		opts := &ebiten.DrawImageOptions{}
+		opts.GeoM.Translate(pad.x, pad.y)
+		if pad.on {
+			screen.DrawImage(pad.imageOn, opts)
+		} else {
+			screen.DrawImage(pad.imageOff, opts)
+		}
+	}
+
+	return nil
 }
 
 func main() {
-  if err := ebiten.Run(update, 320, 240, 2, "Hello Ebiten!"); err != nil {
-    panic(err)
-  }
+	if err := ebiten.Run(update, screenSize, screenSize, 1, "Simple Memory Game"); err != nil {
+		panic(err)
+	}
 }
