15c4c06ea4a6944ee9967463e28d54ecb12e3216
Darren <theinvader360@gmail.com>
2020-05-25 10:30:29 +0100

    Functioning game

--- main.go
+++ main.go
@@ -12,16 +12,15 @@ import (
 )
 
 const (
-	screenSize, padSize = 480, 220
+	screenSize, padSize, minLevel, maxLevel, newLevelDelayTicks, lightTicks, darkTicks = 480, 220, 1, 10, 60, 40, 20
 )
 
 var (
-	targetSequence sequence
-	pads           []pad
-	level          int
-	demo           bool
-	listening      bool
-	tickCounter    int
+	sequence                         []int
+	pads                             []pad
+	lastPressedPad                   *pad
+	demoMode, playMode               bool
+	level, currentIndex, tickCounter int
 
 	blueDark    = &color.NRGBA{0x00, 0x00, 0x33, 0xff}
 	blueLight   = &color.NRGBA{0x00, 0x00, 0xff, 0xff}
@@ -33,11 +32,6 @@ var (
 	yellowLight = &color.NRGBA{0xff, 0xff, 0x00, 0xff}
 )
 
-type sequence struct {
-	values       []int
-	currentIndex int
-}
-
 type pad struct {
 	x, y              float64
 	on                bool
@@ -47,11 +41,10 @@ type pad struct {
 func init() {
 	rand.Seed(time.Now().UnixNano())
 
-	targetSequence.values = make([]int, 30)
-	for index := range targetSequence.values {
-		targetSequence.values[index] = rand.Intn(4)
+	sequence = make([]int, maxLevel)
+	for index := range sequence {
+		sequence[index] = rand.Intn(4)
 	}
-	fmt.Println(targetSequence.values)
 
 	blueDarkImage, _ := ebiten.NewImage(padSize, padSize, ebiten.FilterDefault)
 	blueDarkImage.Fill(blueDark)
@@ -75,49 +68,64 @@ func init() {
 	pads = append(pads, pad{0, screenSize - padSize, false, redDarkImage, redLightImage})
 	pads = append(pads, pad{screenSize - padSize, screenSize - padSize, false, yellowDarkImage, yellowLightImage})
 
-	level = 3
-
-	demo = true
+	level = minLevel - 1
+	nextLevel()
 }
 
 func update(screen *ebiten.Image) error {
-	tickCounter++
-
-	if demo {
-		if targetSequence.currentIndex < level {
+	if demoMode {
+		tickCounter++
+		if currentIndex < level {
 			if tickCounter == 1 {
 				allPadsOff()
-				pads[targetSequence.values[targetSequence.currentIndex]].on = true
+				pads[sequence[currentIndex]].on = true
 			}
-			if tickCounter == 41 {
+			if tickCounter == 1+lightTicks {
 				allPadsOff()
 			}
-			if tickCounter == 61 {
+			if tickCounter == 1+lightTicks+darkTicks {
 				tickCounter = 0
-				targetSequence.currentIndex++
+				currentIndex++
 			}
 		} else {
-			targetSequence.currentIndex = 0
-			demo = false
-			listening = true
+			currentIndex = 0
+			demoMode = false
+			playMode = true
 		}
 	}
 
-	if listening {
+	if playMode {
 		allPadsOff()
+		triggerPad := -1
 		if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
 			posX, posY := ebiten.CursorPosition()
 			pos := image.Point{posX, posY}
-			for index, pad := range pads {
-				if pos.In(pad.imageOff.Bounds().Add(image.Point{int(pad.x), int(pad.y)})) {
-					pads[index].on = true
+			pad := getPadAtPos(&pos)
+			if pad != nil {
+				pad.on = true
+				lastPressedPad = pad
+			} else {
+				triggerPad = releaseLastPressedPad()
+			}
+		} else {
+			triggerPad = releaseLastPressedPad()
+		}
+
+		if triggerPad >= 0 {
+			if sequence[currentIndex] == triggerPad {
+				if currentIndex+1 < level {
+					currentIndex++
+				} else {
+					nextLevel()
 				}
+			} else {
+				gameOver(fmt.Sprintf("LOST AT LEVEL %d", level))
 			}
 		}
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyQ) {
-		os.Exit(0)
+		gameOver("QUIT")
 	}
 
 	if ebiten.IsDrawingSkipped() {
@@ -141,6 +149,46 @@ func allPadsOff() {
 	for index := range pads {
 		pads[index].on = false
 	}
+}
+
+func getPadAtPos(pos *image.Point) *pad {
+	for index, pad := range pads {
+		if pos.In(pad.imageOff.Bounds().Add(image.Point{int(pad.x), int(pad.y)})) {
+			return &pads[index]
+		}
+	}
+	return nil
+}
+
+func releaseLastPressedPad() int {
+	returnVal := -1
+	if lastPressedPad != nil {
+		for index, pad := range pads {
+			if pad == *lastPressedPad {
+				returnVal = index
+			}
+		}
+		lastPressedPad = nil
+	}
+	return returnVal
+}
+
+func nextLevel() {
+	if level == maxLevel {
+		gameOver("COMPLETED")
+	} else {
+		level++
+		currentIndex = 0
+		tickCounter = -newLevelDelayTicks
+		demoMode = true
+		playMode = false
+		fmt.Println(fmt.Sprintf("LEVEL %v", level))
+	}
+}
+
+func gameOver(message string) {
+	fmt.Println(message)
+	os.Exit(0)
 }
 
 func main() {
